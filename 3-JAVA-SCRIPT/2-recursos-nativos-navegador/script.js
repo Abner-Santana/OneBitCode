7c01ed9552faad009961132cd1f1f8a381023ba0
Abner-Santana <123970479+Abner-Santana@users.noreply.github.com>
2025-01-10 07:39:47 -0400

    recursos_do_navegador

---
+++ 3-JAVA-SCRIPT/2-recursos-nativos-navegador/script.js
@@ -0,0 +1,11 @@
+let t = 'name'
+
+//recursos no objeto console
+console.log(t)
+console.error(t)
+console.warn(t)
+
+//recurso no objeto windows (objeto global do navegador)
+    // let name = window.prompt('Insira o seu nome:')
+    // window.alert(name) 
+    // window.confirm('confirma essa ação?')
