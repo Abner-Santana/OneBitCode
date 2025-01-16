7ba64a79b2e6c83d730814a39af5662cd1286f7a
Abner-Santana <123970479+Abner-Santana@users.noreply.github.com>
2025-01-16 11:04:46 -0400

    for

---
+++ 3-JAVA-SCRIPT/15-for/script.js
@@ -0,0 +1,6 @@
+// Um exemplo comum de uso do for é quando queremos iterar um número fixo de vezes como uma vez para cada letra de uma palavra, ou uma vez para cada item de uma lista
+
+let n = 'Abner'
+for(let i = 0; i < n.length; i++){
+    console.log(n[i])
+}
