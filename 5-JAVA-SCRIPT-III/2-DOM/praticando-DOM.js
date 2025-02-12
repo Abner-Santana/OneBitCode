8a7efbc3e436c9305706b0347b9c74ab816f795f
Abner-Santana <123970479+Abner-Santana@users.noreply.github.com>
2025-02-11 22:26:44 -0400

    DOM

--- 5-JAVA-SCRIPT-III/2-DOM/praticando-DOM.js
+++ 5-JAVA-SCRIPT-III/2-DOM/praticando-DOM.js
@@ -1,48 +1,40 @@
-function addContact() {
-    const contactSection = document.getElementById('contacts-list')
-  
-    const h3 = document.createElement('h3')
-    h3.innerText = "Contato"
-  
-    const ul = document.createElement('ul')
-  
-    const nameLi = document.createElement('li')
-    nameLi.innerText = "Nome: "
-    const nameInput = document.createElement('input')
-    nameInput.type = 'text'
-    nameInput.name = 'fullname'
-    nameLi.appendChild(nameInput)
-    ul.appendChild(nameLi)
-    ul.appendChild(document.createElement('br'))
-  
-    const phoneLi = document.createElement('li')
-    phoneLi.innerText = "Telefone: "
-    const phoneInput = document.createElement('input')
-    phoneInput.type = 'text'
-    phoneInput.name = 'phone'
-    phoneLi.appendChild(phoneInput)
-    ul.appendChild(phoneLi)
-    ul.appendChild(document.createElement('br'))
-  
-    const addressLi = document.createElement('li')
-    addressLi.innerHTML = '<label for="address">Endereço: </label>'
-    const addressInput = document.createElement('input')
-    addressInput.type = 'text'
-    addressInput.name = 'address'
-    addressInput.id = 'address'
-    addressLi.appendChild(addressInput)
-    ul.appendChild(addressLi)
-    ul.appendChild(document.createElement('br'))
-  
-    contactSection.append(h3, ul)
-  }
-  
-  function removeContact() {
-    const contactSection = document.getElementById('contacts-list')
-  
-    const titles = document.getElementsByTagName('h3')
-    const contacts = document.getElementsByTagName('ul')
-  
-    contactSection.removeChild(titles[titles.length - 1])
-    contactSection.removeChild(contacts[contacts.length - 1])
-  }
+function addContact(){
+  const contactSection = document.getElementById('contacts-list')
+
+  const h3 = document.createElement('h3')
+  h3.innerText = 'Contato'
+
+
+  const ul = document.createElement('ul')
+
+  const nameLi = document.createElement('li')
+  nameLi.innerText = 'Nome'
+  const nameInput = document.createElement('input')
+  nameInput.type = 'text'
+  nameInput.name = 'fullName'
+
+  nameLi.appendChild(nameInput) //li <- nameInput
+  const br = document.createElement('br')
+  ul.append(nameLi, br) //ul <- li
+
+  const phoneLi = document.createElement('li')
+  phoneLi.innerText = 'Phone'
+  const phoneInput = document.createElement('input')
+  phoneInput.type = 'text'
+  phoneInput.name = 'phone'
+
+  phoneLi.appendChild(phoneInput) //li <- nameInput
+  ul.appendChild(phoneLi) //ul <- li
+
+  contactSection.append(h3, ul)
+}
+
+function removeContact(){
+  const contactSection = document.getElementById('contacts-list')
+
+  const titles = document.getElementsByTagName('h3')
+  const contacts = document.getElementsByTagName('ul')
+
+  contactSection.removeChild(titles[titles.length - 1])
+  contactSection.removeChild(contacts[contacts.length - 1])
+}
